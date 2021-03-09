078daba1805afc549ffa7720e495117da3ea6e98
Roman <kinash0226@gmail.com>
2021-03-09 18:05:11 +0200

    first commit

---
+++ UnitTest1.7/UnitTest1.7.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../1.7/Payment.h"
+#include "../1.7/Payment.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest17
+{
+	TEST_CLASS(UnitTest17)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Payment z;
+			z.Init(4000, 100,5,4000,0);
+			int test = z.³nterestt();
+			Assert::AreEqual(4200, test);
+		}
+	};
+}
